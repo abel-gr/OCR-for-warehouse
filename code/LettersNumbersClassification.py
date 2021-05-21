aedc8f9f6cabc1519df16843258a9bfa2b85b23f
n226 <64910220+n226@users.noreply.github.com>
2021-05-21 12:35:44 +0200

    MLP training improved

--- code/LettersNumbersClassification.py
+++ code/LettersNumbersClassification.py
@@ -1,6 +1,14 @@
 import numpy as np
-from joblib import load
+from joblib import load, dump
 from PIL import Image
+
+import os
+
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
+
+import Mathematical_Morphology as morpho
 
 def getClassIDfromChar(character, CaseSensitive=False):
     
@@ -75,6 +83,88 @@ def getCharFromClassID(code, CaseSensitive=False):
         
     return chr(code)
 
+dataset_images_sizeX = 100
+dataset_images_sizeY = 150
+
+imScales = [0.3, 0.4, 0.5, 0.6, 0.7, 1.0]
+
+# Trains a Multi-Layer perceptron with the letters and numbers images and saves it as a .joblif file
+def train(imgs_path):
+    
+    dicClases = {}
+
+    X = []
+    y = []
+
+    for i in range(1, 6):
+        path = imgs_path + '/font' + str(i) + '/'
+        
+        for x in os.listdir(path):
+
+            for j in imScales:
+
+                img = Image.open(path + x)
+
+                s0 = (int) (dataset_images_sizeX * j)
+                s1 = (int) (dataset_images_sizeY * j)
+
+                img = img.resize((s0, s1))
+                img = np.asarray(img, dtype=np.float32)
+                img = np.mean(img, axis=2)
+                img = np.where(img < 200, 1, 0)
+
+                indices = np.where(img==1)
+
+                y0 = np.min(indices[0]) - 5
+                y1 = np.max(indices[0]) + 5
+                x0 = np.min(indices[1]) - 5
+                x1 = np.max(indices[1]) + 5
+
+
+                if(y0 < 0):
+                    y0 = 0
+
+                if(y1 >= s1):
+                    y1 = s1 - 1
+
+                if(x0 < 0):
+                    x0 = 0
+
+                if(x1 >= s0):
+                    x1 = s0 - 1
+
+                img = img[y0:y1, x0:x1]    
+
+                img = Image.fromarray(img)
+                img = np.asarray(img.resize((dataset_images_sizeX, dataset_images_sizeY)))             
+
+                clase = getClassIDfromChar(x[0], False)
+
+                X.append(img.reshape(-1))
+                y.append(clase)
+
+            if(clase not in dicClases):
+                dicClases[clase] = x[0]
+
+    X = np.asarray(X)
+    TY = np.asarray(y)
+
+    y = np.zeros((TY.shape[0], len(dicClases.keys()) + 1), np.uint8)
+
+    for i, tyVal in enumerate(TY):
+        y[i, tyVal] = 1
+        
+    X, y = shuffle(X, y, random_state=0)
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = 0.8, stratify=y, random_state=1)
+    
+    clf = MLPClassifier(random_state=1, max_iter=2000, hidden_layer_sizes=[400, 200, 200], learning_rate_init=1.0e-3, alpha=0.01)
+    
+    clf.fit(X_train, y_train)
+    
+    dump(clf, 'MLP.joblib')
+    
+    return [clf, X_train, X_test, y_train, y_test]
 
 def ClassifyLettersNumbers(imgs):
     
@@ -83,12 +173,13 @@ def ClassifyLettersNumbers(imgs):
     ln = ''
     
     crow = 1
+    prevMeanVal1 = 0
     
     for im, row, meanval1 in imgs:
 
         im_c = Image.fromarray(im)
-        im_c = np.asarray(im_c.resize((32, 21)))
-        im_c = np.where(im_c == 1, 0, 1)
+        im_c = np.asarray(im_c.resize((dataset_images_sizeX, dataset_images_sizeY)))
+        im_c = np.where(im_c == 1, 1, 0)
 
         y_pred = clf.predict_proba(im_c.reshape(1, -1))
 
@@ -97,8 +188,18 @@ def ClassifyLettersNumbers(imgs):
         lett = getCharFromClassID(clase)
         
         if(row != crow):
+            
             ln = ln + '\n'
             crow = row
+            prevMeanVal1 = 0
+            
+        else:
+            
+            if (meanval1 - prevMeanVal1) > (dataset_images_sizeX * 0.3):
+                
+                ln = ln + ' '
+            
+        prevMeanVal1 = meanval1
         
         ln = ln + lett
         
